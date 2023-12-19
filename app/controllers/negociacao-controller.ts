f09192d9795b5d4f4b57fdc46a17b4c75afc9b76
Raul Avezu <44682390+RaulAvezu@users.noreply.github.com>
2023-12-19 15:49:21 +0000

    aula 4

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -12,17 +12,28 @@ export class NegociacaoController {
     }
 
     adiciona() {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao);
+        this.limparFormulario();
+    }
+
+    criaNegociacao(): Negociacao {
         const exp = /-/g;
         const date = new Date(this.inputData.value.replace(exp, ','));
         const quantidade = parseInt(this.inputQuantidade.value)
         const valor = parseFloat(this.inputValor.value)
-        const negociacao = new Negociacao(
+        return new Negociacao(
             date,
             quantidade,
-            valor
+            valor 
         );
+    }
 
-        console.log(negociacao);
+    limparFormulario(): void{
+        this.inputData.value = '';
+        this.inputQuantidade.value ='';
+        this.inputValor.value = '';
+        this.inputData.focus();
     }
 
 }
